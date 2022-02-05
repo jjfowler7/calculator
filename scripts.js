146ffe0b7b8e816eade6c0bf0bfed2882c21dd95
jjfowler7 <jjfowler7@gmail.com>
2022-02-05 10:21:28 -0500

    Add statment to prevent dividing by 0

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function add(operand1, operand2) {
 };
 
 function subtract(operand1, operand2) {
-    return operand1 - operand2
+    return operand1 - operand2;
 };
 
 function multiply(operand1, operand2) {
@@ -13,6 +13,7 @@ function multiply(operand1, operand2) {
 };
 
 function divide(operand1, operand2) {
+    if(operand2 === 0) return 'Undefined';
     return operand1 / operand2;
 };
 
